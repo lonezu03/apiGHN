ac6283ea57fe734b4a0ff4719871ed9447d2f181
lonezu03 <118800029+lonezu03@users.noreply.github.com>
2025-04-08 22:20:31 +0700

    xx

--- apiGHTK.AppHost/obj/Debug/net8.0/Aspire/references/_AppHost.ProjectMetadata.g.cs
+++ apiGHTK.AppHost/obj/Debug/net8.0/Aspire/references/_AppHost.ProjectMetadata.g.cs
@@ -8,5 +8,5 @@ namespace Projects;
 public class apiGHTK_AppHost
 {
     private apiGHTK_AppHost() { }
-    public static string ProjectPath => """F:\Projec\C#\giaohangtietkiem\apiGHTK\apiGHTK.AppHost""";
+    public static string ProjectPath => """F:\Projec\C#\giaohangtietkiem\apiGHTK\apiGHN\apiGHTK.AppHost""";
 }
--- apiGHTK.ServiceDefaults/obj/Debug/net8.0/apiGHTK.ServiceDefaults.AssemblyInfo.cs
+++ apiGHTK.ServiceDefaults/obj/Debug/net8.0/apiGHTK.ServiceDefaults.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("apiGHTK.ServiceDefaults")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7504716664d9c7cf8431e975fb7521575c1b4ddd")]
 [assembly: System.Reflection.AssemblyProductAttribute("apiGHTK.ServiceDefaults")]
 [assembly: System.Reflection.AssemblyTitleAttribute("apiGHTK.ServiceDefaults")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
